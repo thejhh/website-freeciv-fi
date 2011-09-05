8ce593325229c1755d24969ead98a34068119725
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-06 01:57:47 +0300

    Updated

--- scripts/random-pick-nations-sql.js
+++ scripts/random-pick-nations-sql.js
@@ -29,6 +29,7 @@ tables.reg.select('r.reg_id AS reg_id', 'r.number AS number', 'p.name AS name').
 	foreach(data).do(function(row) {
 		if(row.number > 126) return;
 		if(row.name) return;
+		var number = row.number;
 		var reg_id = row.reg_id;
 		console.log("Choosing nation for player #" + row.number + " by random...");
 		
@@ -37,8 +38,17 @@ tables.reg.select('r.reg_id AS reg_id', 'r.number AS number', 'p.name AS name').
 				console.log('Error: ' + err2);
 				return;
 			}
+			console.log("number = " + sys.inspect(number));
 			console.log("game_id = " + sys.inspect(game_id));
 			console.log("reg_id = " + sys.inspect(reg_id));
+			if(!number) return;
+			if(!( (number >= 1) && (number <= 126) )) return;
+			if(!game_id) return;
+			if(!reg_id) return;
+			if(!leader) return;
+			if(!nation) return;
+			if(!leader.name) return;
+			if(!nation.name) return;
 			console.log("leader = " + sys.inspect(leader.name));
 			console.log("nation = " + sys.inspect(nation.name));
 			tables.player.insert({'game_id':game_id, 'reg_id':reg_id, 'name':leader.name, 'nation':nation.name}, function(err3, player_id) {
