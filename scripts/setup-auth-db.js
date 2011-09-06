6f65944be12eaf1485723e9d63944037b46c4294
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-06 04:24:20 +0300

    Updated

--- scripts/setup-auth-db.js
+++ scripts/setup-auth-db.js
@@ -20,7 +20,7 @@ tables.reg.select('r.reg_id AS reg_id',
 		var number = row.number;
 		var reg_id = row.reg_id;
 		var user_id = row.user_id;
-		var username = (""+name).toLowerCase().replace(/[äÄåÅáàâãä]/g, 'a').replace(/[öÖóòõôö]/g, 'o').replace(/'/g, "").replace(/[^a-z0-9_]+/g, "_").substr(0, 32);
+		var username = (""+name).toLowerCase().replace(/[äÄåÅáàâãä]/g, 'a').replace(/[öÖóòõôö]/g, 'o').replace(/'/g, "").replace(/[^a-z0-9_]+/g, "_").substr(0, 32).replace(/^_+/g, "").replace(/_+$/, "").replace(/_+/g, "_");
 		console.log("Creating account name for player #" + row.number + " as '" + username +"' (from '" + name + "')...");
 		
 		console.log("number = " + sys.inspect(number));
