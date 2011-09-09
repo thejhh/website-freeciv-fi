fe7168ba69c1ff6a761349fdf3e379481a7aa609
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-09 20:02:19 +0300

    Updated

--- freeciv-server/client.js
+++ freeciv-server/client.js
@@ -2,7 +2,8 @@
 
 var io = require('socket.io-client'),
     config = require('./config.js'),
-    socket = io.connect('http://localhost:8000');
+    socket = io.connect('http://localhost:8000'),
+	connected = false;
 
 socket.on('error', function(msg) {
 	console.error('Error: ' + msg);
@@ -13,7 +14,7 @@ socket.on('connect', function () {
 		console.log('Login failed!');
 	});
 	socket.on('login:done', function() {
-		socket.emit('civserver:exec', 'help');
+		connected = true;
 		socket.on('civserver:reply', function (msg) {
 			console.log(msg);
 		});
@@ -21,3 +22,23 @@ socket.on('connect', function () {
 	socket.emit('login', config.authkey);
 });
 
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout),
+    prefix = '$ ';
+rl.on('line', function (cmd) {
+	if(connected) {
+		socket.emit('civserver:exec', cmd);
+	} else {
+		console.log('Error: Not yet connected!');
+	}
+	rl.setPrompt(prefix, prefix.length);
+	rl.prompt();
+}).on('close', function() {
+	console.log('Have a great day!');
+	process.exit(0);
+});
+
+rl.setPrompt(prefix, prefix.length);
+rl.prompt();;
+
+/* EOF */
