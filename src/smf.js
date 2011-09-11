d6ca5c96842bba026b7eda9766c266b6f3db02be
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-11 03:08:05 +0300

    Updated

--- src/smf.js
+++ src/smf.js
@@ -26,47 +26,47 @@ smf.registerMember = function registerMember(args, next) {
 	if(args.password.length < 8) throw new TypeError('password too short!');
 	
 	var values = {
-			'member_name': args.username,
-			'email_address': args.email,
-			'passwd': hash.sha1( args.username.toLowerCase() + args.password ),
-			'password_salt': hash.md5(args.username.toLowerCase() + args.password).substr(0, 4), // AFAIK this isn't even used inside SMF?
-			'posts': 0,
-			'date_registered': new Date(),
-			'member_ip': args.member_ip || '127.0.0.1',
-			'member_ip2': args.member_ip2 || '127.0.0.1',
-			'validation_code': '',
-			'real_name': args.username,
-			'personal_text': '',
-			'pm_email_notify': 1,
-			'id_theme': 0,
-			'id_post_group': 4,
-			'lngfile': '',
-			'buddy_list': '',
-			'pm_ignore_list': '',
-			'message_labels': '',
-			'website_title': '',
-			'website_url': '',
-			'location': '',
-			'icq': '',
-			'aim': '',
-			'yim': '',
-			'msn': '',
-			'time_format': '',
-			'signature': '',
-			'avatar': '',
-			'usertitle': '',
-			'secret_question': '',
-			'secret_answer': '',
-			'additional_groups': '',
-			'ignore_boards': '',
-			'smiley_set': '',
-			'openid_uri': ''
-		};
+		'member_name': args.username,
+		'email_address': args.email,
+		'passwd': hash.sha1( args.username.toLowerCase() + args.password ),
+		'password_salt': hash.md5(args.username.toLowerCase() + args.password).substr(0, 4), // AFAIK this isn't even used inside SMF?
+		'posts': 0,
+		'date_registered': new Date(),
+		'member_ip': args.member_ip || '127.0.0.1',
+		'member_ip2': args.member_ip2 || '127.0.0.1',
+		'validation_code': '',
+		'real_name': args.username,
+		'personal_text': '',
+		'pm_email_notify': 1,
+		'id_theme': 0,
+		'id_post_group': 4,
+		'lngfile': '',
+		'buddy_list': '',
+		'pm_ignore_list': '',
+		'message_labels': '',
+		'website_title': '',
+		'website_url': '',
+		'location': '',
+		'icq': '',
+		'aim': '',
+		'yim': '',
+		'msn': '',
+		'time_format': '',
+		'signature': '',
+		'avatar': '',
+		'usertitle': '',
+		'secret_question': '',
+		'secret_answer': '',
+		'additional_groups': '',
+		'ignore_boards': '',
+		'smiley_set': '',
+		'openid_uri': ''
+	};
 	
 	var keys = [],
 	    placeholders = [];
 	foreach(values).each(function(v, k) {
-		keys.push( k);
+		keys.push(k);
 		placeholders.push( ':' + k);
 	});
 	
